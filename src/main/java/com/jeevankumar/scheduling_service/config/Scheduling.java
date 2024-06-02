e9b6bb2410937bf1ee689476fa476790870c4361
Vinodkumar Reddy <jeevanbhavi440@gmail.com>
2024-06-02 14:29:15 +0530

    second-commit

--- src/main/java/com/jeevankumar/scheduling_service/config/Scheduling.java
+++ src/main/java/com/jeevankumar/scheduling_service/config/Scheduling.java
@@ -1,15 +1,27 @@
 package com.jeevankumar.scheduling_service.config;
 
-import org.springframework.scheduling.annotation.Scheduled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 
 @Component
+@RestController
 public class Scheduling {
+    Logger logger = LoggerFactory.getLogger(Scheduling.class);
 
-    @Scheduled(cron = "0 1 * * * *")
-    public void firstTask(){
-        System.out.println("Task is executed every 1 minute: " + LocalDateTime.now());
+//    @Scheduled(cron = "0 */5 * * * ?")
+//    public void firstTask(){
+//        System.out.println("Task is executed every 1 minute: " + LocalDateTime.now());
+//    }
+    @GetMapping(value = "/log")
+    public void log(){
+    logger.info("this is logger information");
+    logger.warn("This is warn message");
+    logger.debug("This is debug ");
+    logger.error("This is error message");
+    logger.trace("This is trace message");
     }
 }
